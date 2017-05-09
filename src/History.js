9da7f8199e09a8937cea81973bab6ddef26afc04
Kacper Łukawski <ka.lukawski@gmail.com>
2017-05-10 00:34:00 +0200

    Create History class, implemented displaing new entry on page

--- src/History.js
+++ src/History.js
@@ -1,5 +1,27 @@
 class History {
   constructor () {
+    this.history = []
+    this.latest = {}
+  }
 
+  add (entry) {
+    this.history.push(entry)
+    this.latest = entry
+  }
+
+  getLatest () {
+    return this.latest
+  }
+
+  createHTMLElement (element) {
+    var el = document.createElement('div')
+    var month = element.date.getMonth() > 9 ? element.date.getMonth() : `0${element.date.getMonth()}`
+    var day = element.date.getDate() > 9 ? element.date.getDate() : `0${element.date.getDate()}`
+    var hour = element.date.getHours() > 9 ? element.date.getHours() : `0${element.date.getHours()}`
+    var min = element.date.getMinutes() > 9 ? element.date.getMinutes() : `0${element.date.getMinutes()}`
+
+    el.innerHTML = `<p class="upper">pln amount</p> <h1>${element.value}</h1> 
+                    <time>${element.date.getFullYear()}.${month}.${day} ${hour}:${min}</time>`
+    return el
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const converter = new Converter({eur: 4, pln: 4.4})
+const history = new History()
 const convertButton = document.getElementById('convert')
 const convertInput = document.getElementById('value')
+const historySection = document.getElementById('history')
 
 convertButton.addEventListener('click', e => {
   if (!convertInput.value) return false
+  if (isNaN(convertInput.value)) return false
 
   var userValue = Number(convertInput.value)
   var convertedValue = converter.convert('eur', userValue)
-  console.log(convertedValue, Math.random() * 100)
+
+  history.add({ value: convertedValue, date: new Date()})
+
+  var latestEntry = history.getLatest()
+  var newHistoryElement = history.createHTMLElement(latestEntry)
+
+  historySection.appendChild(newHistoryElement)
 })
